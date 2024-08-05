1200957cce83dd8395b9fb73a8cf2f79187fe6cd
Dawid Bazyluk <dawid.bazyluk@gmail.com>
2024-08-06 00:06:36 +0200

    init

---
+++ contacts.js
@@ -0,0 +1,24 @@
+const fs = require("node:fs").promises
+
+
+
+
+
+const contactsPath = "./db/contacts.json";
+
+// TODO: udokumentuj każdą funkcję
+function listContacts() {
+  // ...twój kod
+}
+
+function getContactById(contactId) {
+  // ...twój kod
+}
+
+function removeContact(contactId) {
+  // ...twój kod
+}
+
+function addContact(name, email, phone) {
+  // ...twój kod
+}
