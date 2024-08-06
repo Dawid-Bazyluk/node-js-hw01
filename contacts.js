d0a862bc6c157a0d031d08535a11582ab9297059
Dawid Bazyluk <dawid.bazyluk@gmail.com>
2024-08-06 02:01:42 +0200

    workin

--- contacts.js
+++ contacts.js
@@ -1,24 +1,66 @@
-const fs = require("node:fs").promises
-
-
-
-
+const fs = require("node:fs").promises;
+const { nanoid } = require("nanoid");
+const path = require("node:path");
 
 const contactsPath = "./db/contacts.json";
 
-// TODO: udokumentuj każdą funkcję
 function listContacts() {
-  // ...twój kod
+  const file = fs.readFile(path.resolve(contactsPath));
+  file.then((content) => {
+    const fileStr = content.toString();
+    console.table(JSON.parse(fileStr));
+  });
 }
 
 function getContactById(contactId) {
-  // ...twój kod
+  const file = fs.readFile(path.resolve(contactsPath));
+  file.then((content) => {
+    const fileStr = content.toString();
+    const result = JSON.parse(fileStr);
+    console.log(result.find((contact) => contact.id === contactId));
+  });
 }
 
 function removeContact(contactId) {
-  // ...twój kod
+  const file = fs.readFile(path.resolve(contactsPath));
+  file.then((content) => {
+    const fileStr = content.toString();
+    const result = JSON.parse(fileStr);
+    const afterDelete = result.filter((contact) => contact.id !== contactId);
+    fs.writeFile(path.resolve(contactsPath), JSON.stringify(afterDelete)).then(
+      () => {
+        console.log("The contact has been deleted".green);
+      }
+    );
+  });
 }
 
 function addContact(name, email, phone) {
-  // ...twój kod
+  const file = fs.readFile(path.resolve(contactsPath));
+  file.then((content) => {
+    const fileStr = content.toString();
+      const result = JSON.parse(fileStr);
+      const id = nanoid(21)
+    result.push({
+      id: id,
+      name,
+      email,
+      phone,
+    });
+    fs.writeFile(path.resolve(contactsPath), JSON.stringify(result)).then(
+      () => {
+        console.log(
+          `Added new contact! Name: ${name}, Email: ${email}, Phone: ${phone}, id: ${id}`
+            .green
+        );
+      }
+    );
+  });
 }
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
